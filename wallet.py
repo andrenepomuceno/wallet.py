10cb33d921a53cf16b8cccf64d0b4bdf409c4912
Andre Nepomuceno <andrerm2k@hotmail.com>
2024-01-04 12:19:47 -0300

    Using SQLAlchemy

--- wallet.py
+++ wallet.py
@@ -1,84 +1,119 @@
-#!/usr/bin/python3
-
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import os
 import logging
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wallet.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Define the path for the SQLite database
-DATABASE = 'wallet.db'
+class Investment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    entrada_saida = db.Column(db.String)
+    data = db.Column(db.String)
+    movimentacao = db.Column(db.String)
+    produto = db.Column(db.String)
+    instituicao = db.Column(db.String)
+    quantidade = db.Column(db.Float)
+    preco_unitario = db.Column(db.Float)
+    valor_operacao = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Investment {self.id}>'
+
+def initialize_database():
+    db.create_all()
+
+def process_file(file_path):
+    app.logger.debug(f'Processing file: {file_path}')
+    if file_path.endswith('.csv'):
+        df = pd.read_csv(file_path)
+    elif file_path.endswith('.xlsx'):
+        df = pd.read_excel(file_path)
+
+    df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y').dt.strftime('%Y-%m-%d')
+    df['Preço unitário'] = pd.to_numeric(df['Preço unitário'], errors='coerce').fillna(0.0)
+    df['Valor da Operação'] = pd.to_numeric(df['Valor da Operação'], errors='coerce').fillna(0.0)
+    df['Quantidade'] = pd.to_numeric(df['Quantidade'], errors='coerce').fillna(0.0)
+    return df
+
+def insert_data_into_db(df):
+    for _, row in df.iterrows():
+        # Verifica se a entrada já existe
+        if not Investment.query.filter_by(data=row['Data'], movimentacao=row['Movimentação'],
+                                          produto=row['Produto'], instituicao=row['Instituição']).first():
+            new_entry = Investment(
+                entrada_saida=row['Entrada/Saída'],
+                data=row['Data'],
+                movimentacao=row['Movimentação'],
+                produto=row['Produto'],
+                instituicao=row['Instituição'],
+                quantidade=row['Quantidade'],
+                preco_unitario=row['Preço unitário'],
+                valor_operacao=row['Valor da Operação']
+            )
+            db.session.add(new_entry)
+    db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            app.logger.debug('File upload attempt with no file part in the request.')
-            return render_template('index.html', message='No file part')
-
         file = request.files['file']
-        if file.filename == '':
-            app.logger.debug('File upload attempt without selecting a file.')
-            return render_template('index.html', message='No selected file')
-
         if file:
             filepath = os.path.join('uploads', file.filename)
             file.save(filepath)
-            app.logger.debug(f'File {file.filename} saved to {filepath}.')
+            app.logger.debug(f'File {file.filename} saved at {filepath}.')
 
-            if filepath.endswith('.csv') or filepath.endswith('.xlsx'):
-                try:
-                    if filepath.endswith('.csv'):
-                        df = pd.read_csv(filepath)
-                    elif filepath.endswith('.xlsx'):
-                        df = pd.read_excel(filepath)
-                    app.logger.debug('File read into dataframe successfully.')
+            df = process_file(filepath)
+            insert_data_into_db(df)
 
-                    con = sqlite3.connect(DATABASE)
-                    df.to_sql('investments', con, if_exists='replace', index=False)
-                    con.close()
-                    app.logger.debug('Dataframe saved to SQLite database successfully.')
-
-                    return redirect(url_for('view_table'))
-                except Exception as e:
-                    app.logger.error(f'Error occurred: {e}')
-                    return render_template('index.html', message='An error occurred while processing the file.')
-            else:
-                app.logger.debug('Invalid file format attempted to upload.')
-                return render_template('index.html', message='Invalid file format')
+            return redirect(url_for('view_table'))
+        else:
+            app.logger.debug('No file provided for upload.')
 
     return render_template('index.html', message='')
 
 @app.route('/view', methods=['GET', 'POST'])
 def view_table():
-    con = sqlite3.connect(DATABASE)
-    query = 'SELECT * FROM investments'
+    app.logger.debug('Rendering view table.')
+
+    # Mapeamento dos nomes dos campos do formulário para os nomes das colunas na tabela
+    column_map = {
+        'Quantidade': Investment.quantidade,
+        'Preço unitário': Investment.preco_unitario,
+        'Valor da Operação': Investment.valor_operacao,
+        'Entrada/Saída': Investment.entrada_saida,
+        'Data': Investment.data,
+        'Movimentação': Investment.movimentacao,
+        'Produto': Investment.produto,
+        'Instituição': Investment.instituicao
+    }
+
+    query = Investment.query.order_by(Investment.data.asc())
+
     if request.method == 'POST':
         filters = request.form.to_dict()
-        filter_clauses = []
         for key, value in filters.items():
-            if value:
+            if value and key in column_map:
+                column = column_map[key]
                 if key in ['Quantidade', 'Preço unitário', 'Valor da Operação']:
-                    # Filtragem para campos numéricos
-                    filter_clauses.append(f"{key} = {value}")
+                    query = query.filter(column == value)
                 else:
-                    # Filtragem para campos textuais e de data
-                    filter_clauses.append(f"{key} LIKE '%{value}%'")
-        if filter_clauses:
-            query += ' WHERE ' + ' AND '.join(filter_clauses)
-        app.logger.debug(f'Applying filters: {filter_clauses}')
+                    query = query.filter(column.like(f'%{value}%'))
 
-    df = pd.read_sql_query(query, con)
-    con.close()
-    app.logger.debug('Data fetched from database with applied filters.')
+    result = query.all()
+    # Convertendo o resultado para um DataFrame do pandas
+    df = pd.DataFrame([(d.entrada_saida, d.data, d.movimentacao, d.produto, d.instituicao, d.quantidade, d.preco_unitario, d.valor_operacao) for d in result], 
+                      columns=['Entrada/Saída', 'Data', 'Movimentação', 'Produto', 'Instituição', 'Quantidade', 'Preço unitário', 'Valor da Operação'])
+
     return render_template('view_table.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == '__main__':
+    with app.app_context():
+        initialize_database()
     app.run(debug=True)
-
-
